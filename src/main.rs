4b5df8a7feb852f81c58f03447b5d78c05ac50d5
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-04 20:05:47 -0400

    Implement basic CLArg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+
+/* 
+Usage:
+
+icfmt [extension] [opt-flags] [path]
+*/
+
 fn main() {
-    println!("Hello icfmt!");
+    parse_args();
+}
+
+fn parse_args() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { 
+        println!("USAGE: icfmt [target file-extension] [opt-flags] [path]");
+        return; 
+    }
+    for i in 0..args.len() {
+        if i > 0 {
+            match args[i].as_str() {
+                "txt" => format_txt_files(),
+                _ => format_txt_files(),
+            }
+        }
+    }
+}
+
+fn format_txt_files() {
+    let path_buf = env::current_dir();
+    let path = match path_buf {
+        Ok(p) => {
+            p
+            .to_str()
+            .map(|s| s.to_string())
+            .unwrap_or(String::from("FOUND A BUG LINE 28"))
+        },
+        Err(e) => {
+            println!("There was a problem getting the path: {}", e);
+            String::from("FOUND A BUG LINE 32")
+        },
+    };
+    dbg!(path);
 }
