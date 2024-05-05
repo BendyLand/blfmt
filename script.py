4cc17a4a41a4c1fbccd17d38ea26358d452d5cdd
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-04 21:37:26 -0400

    Create functions to get and validate file extensions

---
+++ script.py
@@ -0,0 +1,10 @@
+file_path = "src/file-extensions.txt"
+lines = []
+
+with open(file_path, "r") as file:
+    lines = file.readlines()
+
+lines.sort()
+
+with open(file_path, "w") as file:
+    file.writelines(lines)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::env;
+use std::fs;
 
 /* 
 Usage:
 
 icfmt [extension] [opt-flags] [path]
 */
+
+fn do_something() {} // placeholder fn
 
 fn main() {
     parse_args();
@@ -13,14 +16,25 @@ fn main() {
 fn parse_args() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 { 
-        println!("USAGE: icfmt [target file-extension] [opt-flags] [path]");
+        println!("USAGE:\nicfmt [target file-extension] [opt-flags] [path]");
         return; 
+    }
+    else if args.len() == 2 {
+        if &args[1] == "help" || &args[1] == "-h" {
+            println!("USAGE:\nicfmt [target file-extension] [opt-flags] [path]\n");
+            println!("Valid file extensions:");
+            display_file_extensions();
+        }
+        else if check_valid_file_extension(&args[1].to_string()) {
+            println!("Please provide a valid directory path.");
+            return;
+        }
     }
     for i in 0..args.len() {
         if i > 0 {
             match args[i].as_str() {
                 "txt" => format_txt_files(),
-                _ => format_txt_files(),
+                _ => do_something(), 
             }
         }
     }
@@ -29,21 +43,56 @@ fn parse_args() {
 fn get_current_dir() -> String {
     let path_buf = env::current_dir();
     let path = match path_buf {
-        Ok(p) => {
-            p
-            .to_str()
-            .map(|s| s.to_string())
-            .unwrap()
+        Ok(ok_path) => {
+            ok_path
+                .to_str()
+                .map(|s| s.to_string())
+                .unwrap()
         },
         Err(e) => {
             println!("There was a problem getting the path: {}", e);
             String::from("PATH-ERROR")
         },
     };
-    path
+    return path;
 }
 
 fn format_txt_files() {
-    let current_dir = get_current_dir();
+    let current_dir = get_current_dir(); // match with PATH-ERROR to check for success
     dbg!(current_dir);
+}
+
+fn check_valid_file_extension(arg: &String) -> bool {
+    let extensions = get_valid_file_extensions();
+    for extension in extensions {
+        if arg == &extension {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn get_valid_file_extensions() -> Vec<String> {
+    let file_result = fs::read_to_string("src/file-extensions.txt");
+    let file = match file_result {
+        Ok(s) => s,
+        Err(e) => {
+            println!("Error reading file: {}", e);
+            String::from("ERROR")
+        }
+    };
+    let lines = {
+        file
+            .lines()
+            .map(|line| line.to_string())
+            .collect::<Vec<String>>()
+    };
+    return lines;
+}
+
+fn display_file_extensions() {
+    let lines = get_valid_file_extensions();
+    for line in lines {
+        println!("{}", line);
+    }
 }
