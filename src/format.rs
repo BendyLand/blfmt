fb492cd72c203c99ce48c61dba57a8a9e3555b33
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-21 19:20:50 -0400

    Move pub functions to top of format.rs

--- src/format.rs
+++ src/format.rs
@@ -4,6 +4,60 @@ use std::fs::{self, File};
 use std::io::Write;
 use std::process::Command;
 
+pub fn format_c_file(path: String) {
+    let contents = fs::read_to_string(path.clone()).unwrap();
+    let lines = contents.split("\n").collect::<Vec<&str>>();
+    let mut sections = group::group_c_file_into_sections(lines);
+    for i in 0..sections.len() {
+        if sections[i].is_empty() {
+            continue;
+        }
+        let original = &sections[i].clone().to_string();
+        sections[i] = format_c_file_group(original.to_owned());
+    }
+    let result = join_c_file_groups(sections).trim_start().to_string();
+    let ok = utils::write_file(path.clone(), result.as_bytes());
+    match ok {
+        Ok(_) => println!("Successfully wrote: {}", path),
+        Err(e) => println!("Error during `format_c_file()`: {}", e),
+    };
+}
+
+pub fn format_txt_file(path: String, opts: options::TxtOpts, opt_titles: &[String]) {
+    let path_clone = path.clone();
+    let file_contents = fs::read_to_string(&path).unwrap();
+    let result = String::new();
+    let (cols, spacing) = (opts.columns, opts.spacing);
+    let paragraphs = group::group_paragraphs(&file_contents, opt_titles);
+    let mut result = Vec::<String>::new();
+    for paragraph in paragraphs {
+        let temp_para = format_paragraph(paragraph, opts);
+        result.push(temp_para);
+    }
+    let mut sep = "\n".to_string();
+    for _ in 0..opts.spacing {
+        sep += "\n";
+    }
+    let paragraphs = result.join(sep.as_str());
+    let ok = utils::write_file(path, paragraphs.as_bytes());
+    match ok {
+        Ok(_) => println!("Successfully wrote: {}", path_clone),
+        Err(e) => println!("Error during `format_txt_file()`: {}", e),
+    };
+}
+
+pub fn format_go_file(path: String) {
+    let command = format!("gofmt -w {}", path);
+    let res = Command::new("sh").arg("-c").arg(command).output().unwrap();
+    let err = String::from_utf8(res.stderr).unwrap_or_default();
+    if err.as_str() != "" {
+        eprintln!("Error running gofmt:\n{}", err);
+    }
+    else {
+        println!("Go file formatted successfully!");
+    }
+}
+
 pub fn format_rs_file(path: String) {
     let contents = fs::read_to_string(path.clone()).unwrap();
     let lines = contents.split("\n").collect::<Vec<&str>>();
@@ -15,6 +69,26 @@ pub fn format_rs_file(path: String) {
         let original = &sections[i].clone().to_string();
         sections[i] = format_rs_file_group(original.to_owned());
     }
+}
+
+pub fn format_cpp_file(path: String) {
+    let contents = fs::read_to_string(path.clone()).unwrap();
+    let lines = contents.split("\n").collect::<Vec<&str>>();
+    let mut sections = group::group_cpp_file_into_sections(lines);
+    for i in 0..sections.len() {
+        if sections[i].is_empty() {
+            continue;
+        }
+        let original = &sections[i].clone().to_string();
+        sections[i] = format_cpp_file_group(original.to_owned());
+    }
+    // c_file_groups should still work on cpp files
+    let result = join_c_file_groups(sections).trim_start().to_string(); 
+    let ok = utils::write_file(path.clone(), result.as_bytes());
+    match ok {
+        Ok(_) => println!("Successfully wrote: {}", path),
+        Err(e) => println!("Error during `format_c_file()`: {}", e),
+    };
 }
 
 fn format_rs_file_group(section: String) -> String {
@@ -46,28 +120,6 @@ fn format_rs_file_group(section: String) -> String {
         println!("{}", &section);
     }
     return section;
-}
-
-//? In cpp file, if there is a one liner inside of an indented group, 
-//? it will force the following lines to be improperly indented for the remainder of the block.
-pub fn format_cpp_file(path: String) {
-    let contents = fs::read_to_string(path.clone()).unwrap();
-    let lines = contents.split("\n").collect::<Vec<&str>>();
-    let mut sections = group::group_cpp_file_into_sections(lines);
-    for i in 0..sections.len() {
-        if sections[i].is_empty() {
-            continue;
-        }
-        let original = &sections[i].clone().to_string();
-        sections[i] = format_cpp_file_group(original.to_owned());
-    }
-    // c_file_groups should still work on cpp files
-    let result = join_c_file_groups(sections).trim_start().to_string(); 
-    let ok = utils::write_file(path.clone(), result.as_bytes());
-    match ok {
-        Ok(_) => println!("Successfully wrote: {}", path),
-        Err(e) => println!("Error during `format_c_file()`: {}", e),
-    };
 }
 
 fn format_cpp_top_level_group(group: String) -> String {
@@ -244,25 +296,6 @@ fn format_preprocessor_group(group: String) -> String {
         }
     }
     return result;
-}
-
-pub fn format_c_file(path: String) {
-    let contents = fs::read_to_string(path.clone()).unwrap();
-    let lines = contents.split("\n").collect::<Vec<&str>>();
-    let mut sections = group::group_c_file_into_sections(lines);
-    for i in 0..sections.len() {
-        if sections[i].is_empty() {
-            continue;
-        }
-        let original = &sections[i].clone().to_string();
-        sections[i] = format_c_file_group(original.to_owned());
-    }
-    let result = join_c_file_groups(sections).trim_start().to_string();
-    let ok = utils::write_file(path.clone(), result.as_bytes());
-    match ok {
-        Ok(_) => println!("Successfully wrote: {}", path),
-        Err(e) => println!("Error during `format_c_file()`: {}", e),
-    };
 }
 
 fn format_c_file_group(group: String) -> String {
@@ -470,37 +503,3 @@ fn format_paragraph(paragraph: String, opts: options::TxtOpts) -> String {
     return result;
 }
 
-pub fn format_txt_file(path: String, opts: options::TxtOpts, opt_titles: &[String]) {
-    let path_clone = path.clone();
-    let file_contents = fs::read_to_string(&path).unwrap();
-    let result = String::new();
-    let (cols, spacing) = (opts.columns, opts.spacing);
-    let paragraphs = group::group_paragraphs(&file_contents, opt_titles);
-    let mut result = Vec::<String>::new();
-    for paragraph in paragraphs {
-        let temp_para = format_paragraph(paragraph, opts);
-        result.push(temp_para);
-    }
-    let mut sep = "\n".to_string();
-    for _ in 0..opts.spacing {
-        sep += "\n";
-    }
-    let paragraphs = result.join(sep.as_str());
-    let ok = utils::write_file(path, paragraphs.as_bytes());
-    match ok {
-        Ok(_) => println!("Successfully wrote: {}", path_clone),
-        Err(e) => println!("Error during `format_txt_file()`: {}", e),
-    };
-}
-
-pub fn format_go_file(path: String) {
-    let command = format!("gofmt -w {}", path);
-    let res = Command::new("sh").arg("-c").arg(command).output().unwrap();
-    let err = String::from_utf8(res.stderr).unwrap_or_default();
-    if err.as_str() != "" {
-        eprintln!("Error running gofmt:\n{}", err);
-    }
-    else {
-        println!("Go file formatted successfully!");
-    }
-}
